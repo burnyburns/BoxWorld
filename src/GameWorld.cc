10dc1ef5d0ad602248093815e4751c7b11cffefc
burnyburns <phillipworld@hotmail.co.uk>
2015-11-29 19:56:13 +0000

    Test

---
+++ src/GameWorld.cc
@@ -0,0 +1,10 @@
+#include "GameWorld.h"
+
+GameWorld::GameWorld (ApplicationMode mode) {
+  asset_manager = std::make_shared<GameAssetManager>(mode);
+  asset_manager->AddAsset(std::make_shared<CubeAsset>());
+}
+
+void GameWorld::Draw() {
+  asset_manager->Draw();
+}
